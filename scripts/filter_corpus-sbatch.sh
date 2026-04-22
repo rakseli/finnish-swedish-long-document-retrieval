1c8eeb167647b2ca7ad5efde9c079d32218236de
Akseli Reunamo <akseli.reunamo@gmail.com>
2026-04-22 13:35:28 +0300

    slurm

--- scripts/filter_corpus-sbatch.sh
+++ scripts/filter_corpus-sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=filter_corpus
 #SBATCH --account=project_2018556
-#SBATCH --time=03:00:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1                   
 #SBATCH --ntasks-per-node=1     
 #SBATCH --cpus-per-task=1
@@ -14,6 +14,7 @@ echo "Start $(date +"%Y-%m-%d-%H:%M:%S")"
 
 module load python-data
 INPUT_ROOT=$1
+PROCESSING_MODE=$2
 echo "Using input root: $INPUT_ROOT"
-srun python /scratch/project_2018556/finnish-swedish-long-document-retrieval/src/filter_corpus.py --input_root $INPUT_ROOT
+srun python /scratch/project_2018556/finnish-swedish-long-document-retrieval/src/filter_corpus.py --input_root $INPUT_ROOT --mode $PROCESSING_MODE
 echo "End $(date +"%Y-%m-%d-%H:%M:%S")"
